15cacebfe713c9a0237cbc4213e1ce40b1979d9e
anapgh <ap.garcia.2016@alumnos.urjc.es>
2019-09-30 15:21:01 +0200

    Corregido con pep8

--- calcoo.py
+++ calcoo.py
@@ -7,21 +7,20 @@ Created on Mon Sep 23 10:45:26 2019
 """
 import sys
 
-class Calculadora(): #clase madre
-    
 
+class Calculadora():  # clase madre
     def __init__(self, operando1, operator, operando2):
         "Esto es el método iniciliazador"
         self.operando1 = operando1
         self.operator = operator
         self.operando2 = operando2
-    
+
     def suma(self):
         return self.operando1 + self.operando2
-    
+
     def resta(self):
         return self.operando1 - self.operando2
-    
+
     def operar(self):
         if self.operator == "suma":
             return self.suma()
@@ -29,26 +28,21 @@ class Calculadora(): #clase madre
             return self.resta()
         else:
             return("Operacion no valida")
-    
-if __name__ == "__main__":
-    print(sys.argv)
-    if len(sys.argv) != 4:
-        sys.exit("Usage: calcoo.py operando1 operator operando2")
-    
-    operacion = (sys.argv[2])
-        
-    try: 
-        operando1 = float(sys.argv[1])
-        operando2 = float(sys.argv[3])   
-    except ValueError:
-        print("Solo se puede enteros y floats")
-        
-    #creo mi objeto con los argumentos cogidos por pantalla    
-    calcu = Calculadora(operando1, operacion, operando2)   
-    resultado = calcu.operar()
-    print(resultado)
-    
-    
-    
-    
-    
+
+    if __name__ == "__main__":
+        print(sys.argv)
+        if len(sys.argv) != 4:
+            sys.exit("Usage: calcoo.py operando1 operator operando2")
+
+        operacion = (sys.argv[2])
+
+        try:
+            operando1 = float(sys.argv[1])
+            operando2 = float(sys.argv[3])
+        except ValueError:
+            print("Solo se puede enteros y floats")
+
+        # creo mi objeto con los argumentos cogidos por pantalla
+        calcu = Calculadora(operando1, operacion, operando2)
+        resultado = calcu.operar()
+        print(resultado)
